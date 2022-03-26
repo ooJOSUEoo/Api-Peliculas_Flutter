46ed3002e3baf1f62084dd8a57f919cae0f94d0c
Josue <sjosue5082002@gmail.com>
2022-03-26 17:18:44 -0600

    Swiper - parte 1

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,11 +1,45 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text('Hola'),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Películas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 10.0),
+        child: Column(
+          children: <Widget>[
+            _swiperTarjetas(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _swiperTarjetas() {
+    return Container(
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: 200.0,
+        itemBuilder: (BuildContext context,int index){
+          return Image.network("https://via.placeholder.com/350x150",fit: BoxFit.fill,);
+        },
+        itemCount: 3,
+        //pagination: SwiperPagination(),
+        //control: SwiperControl(),
       ),
     );
   }
